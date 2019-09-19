df1514a9ed3646d069933306d29e1d91b3743d4c
Luiz Motter Neto <luiz.motter@gmail.com>
2019-09-19 12:06:41 -0300

    aula 61

---
+++ gulpTasks/server.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp')
+
+gulp.task('watch', () => {}) // sempre que alterar um arquivo, o server é regerado automáticamente.
+
+gulp.task('server', ['watch'], () => {
+    
+})
+
